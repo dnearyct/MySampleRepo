4d8e838052956733676912c53ff4ce1e8653209f
Neary <don10001@ad.uconn.edu>
2019-09-30 14:29:54 -0400

    Adding my Ruby folder

---
+++ Ruby/giraffe.rb
@@ -0,0 +1,18 @@
+character_name = "Colin"
+
+print "There once was a student named " + character_name.downcase()
+puts " who didn't really like to work"
+puts ("But there is a " + character_name.upcase() +
+      " who does like to work somewhere")
+null_value = nil
+boolean_true = true
+boolean_false = false
+puts ( "is " + character_name  + "= Fred, true or false ")
+puts  character_name.include? "Fred"
+test_num = 2**5  # raises 2 ^ 5 #
+absolute_num = -20.487
+puts ( "absolute_num = " + absolute_num.abs().to_s )
+puts ( "my fav num " + test_num.to_s )
+# Math Classes are invoked as follows
+sqrt_number = -31.5
+puts ("Math.sqrt =" + Math.sqrt( sqrt_number ).to_s )
